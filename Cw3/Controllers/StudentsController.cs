0f58563f9a29730cdb5e97201b2615739cdd9968
Przemyslaw Zagorski <s9501@pjwstk.edu.pl>
2020-04-19 16:41:01 +0200

    Zadanie 8

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.DAL;
 using Cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,16 @@ namespace Cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudents(string orderBy)
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
         }
 
         [HttpPost]
---
+++ Cw3/DAL/MockDbService.cs
@@ -0,0 +1,28 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent = 1, FirstName = "Jan", LastName = "Kowalski"},
+                new Student{IdStudent = 2, FirstName = "Anna", LastName = "Malewski"},
+                new Student{IdStudent = 3, FirstName = "Anderzej", LastName = "Andrzejewicz"}
+            };            
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
