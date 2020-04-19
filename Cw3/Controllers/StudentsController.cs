c37bcbc51723e8b5547e2804a5f107240956ff4e
Przemyslaw Zagorski <s9501@pjwstk.edu.pl>
2020-04-19 15:41:01 +0200

    Zadanie 6

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cw3.Controllers
@@ -14,6 +15,13 @@ namespace Cw3.Controllers
         public string GetStudents(string orderBy)
         {
             return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
         }
     }
 }
