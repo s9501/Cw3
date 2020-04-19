8a4a2426347be1484ab9b2bf919b1bea67d2a033
Przemyslaw Zagorski <s9501@pjwstk.edu.pl>
2020-04-19 15:16:47 +0200

    Zadanie 3

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
