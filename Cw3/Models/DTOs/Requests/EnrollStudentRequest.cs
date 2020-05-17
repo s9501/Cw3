e5cb300ef500693d54d7fcfaace6dc3f77d41822
Przemyslaw Zagorski <s9501@pjwstk.edu.pl>
2020-05-17 18:20:15 +0200

    Ćw 5.0

---
+++ Cw3/Models/DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Models.DTOs.Requests
+{
+    public class EnrollStudentRequest
+    {
+        //[RegularExpression("^s[0-9]+$")]
+        public string IndexNumber { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        public string Studies { get; set; }
+    }
+}
---
+++ Cw3/Models/DTOs/Responses/EnrollStudentResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Models.DTOs.Responses
+{
+    public class EnrollStudentResponse
+    {
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Semester { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
